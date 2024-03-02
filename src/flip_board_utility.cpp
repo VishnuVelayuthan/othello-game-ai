0e197017627d2c9337678bc31a7cef697cf79bb5
VishnuVelayuthan <vishnuvelayuthan@gmail.com>
2024-03-01 19:57:41 -0800

    And i believe, flip is done

--- src/flip_board_utility.cpp
+++ src/flip_board_utility.cpp
@@ -1,5 +1,9 @@
 #include "../include/flip_board_utility.h"
 
+#include <algorithm>
+
+using namespace std;
+
 void flipPiecesUp(Tile*** g_board, int b_size, Move* n_move) {
 
     int flip_row = n_move->getIrow();
@@ -88,12 +92,12 @@ void flipPiecesLUD(Tile*** g_board, int b_size, Move* n_move) {
     int flip_col = n_move->getIcol();
     char flip_player = n_move->getPlayer();
 
-    if (flip_col >= b_size - 2) 
+    if (min(flip_col, flip_row) <= 1) 
         return;
 
     Tile* curr_tile; 
-    for (int i = 0; i < b_size - flip_col; i++) {
-        curr_tile = g_board[flip_row + i][flip_col + i];
+    for (int i = 0; i < min(flip_col, flip_row); i++) {
+        curr_tile = g_board[flip_row - i][flip_col - i];
 
         if (curr_tile->getPlayerOcc() == flip_player) 
             return;
@@ -105,9 +109,56 @@ void flipPiecesLUD(Tile*** g_board, int b_size, Move* n_move) {
 
 
 void flipPiecesRUD(Tile*** g_board, int b_size, Move* n_move) {
+    int flip_row = n_move->getIrow();
+    int flip_col = n_move->getIcol();
+    char flip_player = n_move->getPlayer();
+
+    if (min(b_size - flip_col, flip_row) <= 1) 
+        return;
+
+    Tile* curr_tile; 
+    for (int i = 0; i < min(b_size - flip_col, flip_row); i++) {
+        curr_tile = g_board[flip_row - i][flip_col + i];
+
+        if (curr_tile->getPlayerOcc() == flip_player) 
+            return;
+        else
+            curr_tile->setPlayerOcc(flip_player); 
+    }
 }
 void flipPiecesLDD(Tile*** g_board, int b_size, Move* n_move) {
+    int flip_row = n_move->getIrow();
+    int flip_col = n_move->getIcol();
+    char flip_player = n_move->getPlayer();
+
+    if (min(flip_col, b_size - flip_row) <= 1) 
+        return;
+
+    Tile* curr_tile; 
+    for (int i = 0; i < min(flip_col, b_size - flip_row); i++) {
+        curr_tile = g_board[flip_row + i][flip_col - i];
+        if (curr_tile->getPlayerOcc() == flip_player) 
+            return;
+        else
+            curr_tile->setPlayerOcc(flip_player); 
+    }
 }
 void flipPiecesRDD(Tile*** g_board, int b_size, Move* n_move) {
+
+    int flip_row = n_move->getIrow();
+    int flip_col = n_move->getIcol();
+    char flip_player = n_move->getPlayer();
+
+    if (min(b_size - flip_col, b_size - flip_row) <= 1) 
+        return;
+
+    Tile* curr_tile; 
+    for (int i = 0; i < min(b_size - flip_col, b_size - flip_row); i++) {
+        curr_tile = g_board[flip_row + i][flip_col + i];
+        if (curr_tile->getPlayerOcc() == flip_player) 
+            return;
+        else
+            curr_tile->setPlayerOcc(flip_player); 
+    }
 }
 
